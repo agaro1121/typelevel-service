2ed759eae0c306266a1323a1a791a1ab19f7a816
Anthony Garo <agaro1121@gmail.com>
2018-03-13 11:05:47 -0400

    cleans up healthcheck

--- src/main/scala/routes/HeathCheck.scala
+++ src/main/scala/routes/HeathCheck.scala
@@ -4,15 +4,11 @@ import cats.Monad
 import org.http4s.HttpService
 import org.http4s.dsl.Http4sDsl
 
-final class HeathCheck[F[_]: Monad] extends Http4sDsl[F] {
-
-  val healthCheckService: HttpService[F] = HttpService[F] {
-    case GET -> Root =>
-      Ok("Up and Running...")
-  }
-
-}
-
 object HealthCheck {
-  def apply[F[_]: Monad]() = new HeathCheck[F]().healthCheckService
+  def apply[F[_]: Monad]() = new Http4sDsl[F]{
+    val healthCheckService: HttpService[F] = HttpService[F] {
+      case GET -> Root =>
+        Ok("Up and Running...")
+    }
+  }.healthCheckService
 }
